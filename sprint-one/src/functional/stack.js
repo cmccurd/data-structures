55137a3667e092dd22984256c28738bd9314170b
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-09-22 17:07:40 -0500

    finished functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,21 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    return storage[someInstance.size() + 1] = value;
   };
 
   someInstance.pop = function() {
+    var result = storage[someInstance.size()];
+    delete storage[someInstance.size()];
+    return result;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (var key in storage) {
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
