61a07e780737563d6aec2e9ed5633302b7044c44
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-09-23 15:39:38 -0500

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
+  //REMOVE HEAD
   list.removeHead = function() {
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
+  //CONTAINS
   list.contains = function(target) {
+    var result = false;
+    var innerFunction = function(node) {
+      if (node.value === target) {
+        result = true;
+        return;
+      } else if (node.next === null) {
+        return;
+      } else {
+        innerFunction(node.next);
+      }
+    };
+    innerFunction(list.head);
+    return result;
   };
 
   return list;
@@ -26,4 +51,6 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Contains - Linear O(n)
+ * Everything else - Constant - O(1)
  */
