5f285dc6d0a02848664c3a3824fa9a913e449beb
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-09-23 13:46:28 -0500

    completed all tests for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    return this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  //values are uniq to the set storage
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    var result = this._storage[item];
+    delete this._storage[item];
+    return result;
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * CONSTANT TIME ALGORITHM
+ * O(1)
  */
