a324c70096ffc5d0250c51765378e642b26b9999
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-09-23 11:49:35 -0500

    finished es6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
-}
+  enqueue(val) {
+    return this.storage[this.size() + 1] = val;
+  }
+
+  dequeue() {
+    var result = this.storage['1'];
+    delete this.storage['1'];
+    var tempStorage = {};
+    for (var key in this.storage) {
+      tempStorage[key - 1] = this.storage[key];
+    }
+    this.storage = tempStorage;
+    return result;
+  }
+
+  size() {
+    var counter = 0;
+    for (var key in this.storage) {
+      counter++;
+    }
+    return counter;
+  }
+};
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
-}
+  push(val) {
+    return this.storage[this.size() + 1] = val;
+  }
+
+  pop() {
+    var result = this.storage[this.size()];
+    delete this.storage[this.size()];
+    return result;
+  }
+
+  size() {
+    var counter = 0;
+    for (var key in this.storage) {
+      counter++;
+    }
+    return counter;
+  }
+};
