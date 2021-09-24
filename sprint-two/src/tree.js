d2d975174d6c3f78625d3d97b234128cbe8469aa
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-09-23 19:42:35 -0500

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,34 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];// fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var innerFunction = function (nodes) {
+    nodes.forEach(function (node) {
+      if (node.value === target) {
+        result = true;
+        return true;
+      }
+      if (node.children) {
+        innerFunction(node.children);
+      }
+      return false;
+    });
+  };
+  innerFunction(this.children);
+  return result;
 };
-
 
 
 /*
